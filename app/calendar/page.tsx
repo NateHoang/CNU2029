aeb0898b6093ef735f2099a2c212f79854bf4786
Nathan Hoang <114448980+NateHoang@users.noreply.github.com>
2025-09-09 09:14:45 -0700

    fixed calendar

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,8 +5,10 @@ import Iframe from "react-iframe";
 
 const calendars = {
   "CNUCDM 2029": "https://calendar.google.com/calendar/embed?src=cnucdm2029%40gmail.com&ctz=America%2FLos_Angeles",
-  "ADEA": "https://calendar.google.com/calendar/embed?src=0a9869a58c86534363bd63287f2d73b06f61d4cc7e79a2f345bb0834bd380845%40group.calendar.google.com&ctz=America%2FLos_Angeles",
-  "ASDA": "https://calendar.google.com/calendar/embed?src=0a9869a58c86534363bd63287f2d73b06f61d4cc7e79a2f345bb0834bd380845%40group.calendar.google.com&ctz=America%2FLos_Angeles",
+  "ADEA": "https://calendar.google.com/calendar/embed?src=37a82b51382561aa4b4c5d7b97a3dbb0578a27b8521b9fb196bd1fdcaea8fb5c%40group.calendar.google.com&ctz=America%2FLos_Angeles",
+  "ASDA": "https://calendar.google.com/calendar/embed?src=29a39d539f4fc03b3a3987051fa8bc6ef18056d2855cf211190332044fc3e4f8%40group.calendar.google.com&ctz=America%2FLos_Angeles",
+  "CDA": "https://calendar.google.com/calendar/embed?src=76f8fbe1f9aefd4068a1f0b3f52a454e7bd4214655143a0dcf7aee6a627c665d%40group.calendar.google.com&ctz=America%2FLos_Angeles",
+  "DSD": "https://calendar.google.com/calendar/embed?src=5c1f86ab8861c898dd5fe8f041fe31eef052f632326568d7c211f4397da55449%40group.calendar.google.com&ctz=America%2FLos_Angeles",
 };
 
 const calendarNames = Object.keys(calendars) as (keyof typeof calendars)[];
