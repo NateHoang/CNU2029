a4445c575f135f8952c97ad5a47b58d093c4a346
Nathan Hoang <114448980+NateHoang@users.noreply.github.com>
2026-09-23 15:20:25 -0700

    fix-update eslint to 9.0, and added analytics to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Main from "../components/main";
 import Events from "../components/events";
+import { Analytics } from "@vercel/analytics/next";
 
 const Home = () => {
   return (
     <div className="">
+      <Analytics />
       <Main></Main>
       <Events></Events>
     </div>
